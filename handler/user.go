9b1376044c33837454fae0bfca47d8763b822504
Dimitri Irfan <dimitriirfan@gmail.com>
2022-07-15 16:08:27 +0700

    added login endpoint

--- handler/user.go
+++ handler/user.go
@@ -25,15 +25,15 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 		errors := helper.FormatError(err)
 		errorMessage := gin.H{"errors": errors}
 
-		response := helper.APIResponse("Register account failed", http.StatusUnprocessableEntity, "success", errorMessage)
-		c.JSON(http.StatusBadRequest, response)
+		response := helper.APIResponse("Register account failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
 		return
 	}
 
 	newUser, err := h.userService.RegisterUser(input)
 
 	if err != nil {
-		response := helper.APIResponse("Register account failed", http.StatusBadRequest, "success", nil)
+		response := helper.APIResponse("Register account failed", http.StatusBadRequest, "error", nil)
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
@@ -42,6 +42,35 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 
 	formatter := user.FormatUser(newUser, "tokentoken")
 	response := helper.APIResponse("Account successfully registered", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+
+}
+
+func (h *userHandler) Login(c *gin.Context) {
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Login account failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+	}
+
+	loggedinUser, err := h.userService.Login(input)
+
+	if err != nil {
+		errors := helper.FormatError(err)
+		errorMessage := gin.H{"errors": errors}
+		response := helper.APIResponse("Login account failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+	}
+
+	formatter := user.FormatUser(loggedinUser, "tokentoken")
+
+	response := helper.APIResponse("Successfully log in", http.StatusOK, "success", formatter)
 
 	c.JSON(http.StatusOK, response)
 
