b719f3b72d5107fa5da3107d052558b78c1cce18
Dimitri Irfan <dimitriirfan@gmail.com>
2022-07-15 19:09:32 +0700

    added upload avatar endpoint

--- main.go
+++ main.go
@@ -24,6 +24,8 @@ func main() {
 	userService := user.NewService(userRepository)
 	userHandler := handler.NewUserHandler(userService)
 
+	userService.SaveAvatar(1, "images/1-profile.png")
+
 	router := gin.Default()
 
 	api := router.Group("/api/v1")
@@ -31,6 +33,7 @@ func main() {
 	api.POST("/users", userHandler.RegisterUser)
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
+	api.POST("/avatars", userHandler.UploadAvatar)
 
 	router.Run()
 
