8bf8b0221e987c45b901b4fa85598c461f98fd72
Dimitri Irfan <dimitriirfan@gmail.com>
2022-07-20 19:57:18 +0700

    added static routing for image

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -1,1 +1,40 @@
 package campaign
+
+type CampaignFormatter struct {
+	ID               int    `json:"id"`
+	UserID           int    `json:"user_id"`
+	Name             string `json:"name"`
+	ShortDescription string `json:"short_description"`
+	ImageURL         string `json:"image_url"`
+	GoalAmmount      int    `json:"goal_amount"`
+	CurrentAmmount   int    `json:"current_amount"`
+	Slug             string `json:"slug"`
+}
+
+func FormatCampaign(campaign Campaign) CampaignFormatter {
+	formatter := CampaignFormatter{
+		ID:               campaign.ID,
+		UserID:           campaign.UserID,
+		Name:             campaign.Name,
+		ShortDescription: campaign.ShortDescription,
+		GoalAmmount:      campaign.GoalAmmount,
+		CurrentAmmount:   campaign.CurrentAmmount,
+		Slug:             campaign.Slug,
+		ImageURL:         "",
+	}
+
+	if len(campaign.CampaignImages) > 0 {
+		formatter.ImageURL = campaign.CampaignImages[0].FileName
+	}
+
+	return formatter
+}
+
+func FormatCampaigns(campaigns []Campaign) []CampaignFormatter {
+	campaignsFormatter := []CampaignFormatter{}
+	for _, campaign := range campaigns {
+		campaignsFormatter = append(campaignsFormatter, FormatCampaign(campaign))
+	}
+
+	return campaignsFormatter
+}
